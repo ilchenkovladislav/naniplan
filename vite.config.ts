8f61888f575e99693bb19985d3c2c3b5a6d72dda
Ilchenko <ilchenkowd@gmail.com>
2025-06-14 16:43:49 +0300

    изменение vite конфига

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
