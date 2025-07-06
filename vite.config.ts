638d152919fc6262382800228a4e214e887a4dd5
Ilchenko <ilchenkowd@gmail.com>
2025-07-06 15:46:51 +0300

    добавил pwa и всё необходимое

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const vitePwaPlugin = VitePWA({
+  registerType: 'autoUpdate',
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+  },
+  manifest: {
+    name: 'Nani Planner',
+    short_name: 'NaniPlan',
+    description: 'Nani Planner - Your Personal Task Manager',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+})
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +37,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
   },
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), vitePwaPlugin],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
