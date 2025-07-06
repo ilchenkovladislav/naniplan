907d4f68dc3355dcd32e11edc46720f562c3bd24
Ilchenko <ilchenkowd@gmail.com>
2025-07-06 18:37:40 +0300

    исправление deploy ссылок

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import YearView from '@/pages/YearView/YearView.vue'
 import MyEditor from '@/components/MyEditor/MyEditor.vue'
 
 const router = createRouter({
-  history: createWebHistory('/naniplan/'),
+  history: createWebHashHistory('/naniplan'),
   routes: [
     {
       path: '/',
